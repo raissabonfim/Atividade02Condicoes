90cbf634754ddd2364dd875a15ce67dbe4a430ab
JonathanBrunetto <168039735+JonathanBrunetto@users.noreply.github.com>
2024-08-28 19:53:39 -0300

    Atividade1

---
+++ src/Atividade01.java
@@ -0,0 +1,11 @@
+public class Atividade01 {
+    public static void main(String[] args) {
+
+        int id = 12;
+        if (id >= 18) {
+            System.out.println("você é maior de idade ");
+        }else {
+            System.out.println("você é menor de idade ");
+        }
+    }
+}
