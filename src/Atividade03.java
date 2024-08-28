9c3533149f57de04401d5a85d46e3ed649c58ba4
JonathanBrunetto <168039735+JonathanBrunetto@users.noreply.github.com>
2024-08-28 20:13:18 -0300

    Atividade1

---
+++ src/Atividade03.java
@@ -0,0 +1,11 @@
+public class Atividade03 {
+    public static void main(String[] args) {
+        // verificar se ímpar ou par
+        int num =8;
+        if (num  %2 == 0  ){
+            System.out.println("É par");
+        }else {
+            System.out.println("É impar");
+        }
+    }
+}
